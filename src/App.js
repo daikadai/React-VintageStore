641660a36090a03b64a3f4e88350ac5b911a24c5
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-05-07 13:57:42 +0700

    2. Product Details Page, Header

--- src/App.js
+++ src/App.js
@@ -10,23 +10,38 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
+import Header from "./components/Header";
 // components
 
 export default function App() {
-  return <Router>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/cart">
-        <Cart />
-      </Route>
-      <Route path="*">
-        <Error />
-      </Route>
-    </Switch>
-  </Router>
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:id" children={<ProductDetails></ProductDetails>}>
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
