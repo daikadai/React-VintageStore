3e179793f1d382ab831bf7e530e0c232091c5197
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-05-07 14:20:30 +0700

    3. Error Page, About Page, Hero Component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return <section className="section about-section">
+    <h1 className="section-title">about us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore vero eos adipisci voluptas voluptatum iste autem enim id quod. Ducimus magnam optio in vero, voluptatum, ullam obcaecati vel nesciunt perspiciatis iusto natus quo culpa nostrum suscipit laudantium quae. Iste, soluta?</p>
+  </section>
 }
