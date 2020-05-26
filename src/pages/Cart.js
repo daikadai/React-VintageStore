4f7bfa6aa1a0800222ec671e7146baea7b667bfc
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-05-26 17:19:51 +0700

    10. Empty Cart, Cart Page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { CartContext } from "../context/cart";
+import EmptyCart from "../components/Cart/EmptyCart";
+import CartItem from "../components/Cart/CartItem";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
+  let user = false;
   const { cart, total } = React.useContext(CartContext);
-  console.log({ cart, total });
 
-  return <h1>hello from cart page</h1>
+  if (cart.length === 0) {
+    return <EmptyCart />
+  }
+
+  return <section className="cart-items section">
+    <h2>your cart</h2>
+    {
+      cart.map(item => {
+        return <CartItem key={item.id} {...item} />
+      })
+    }
+    <h2>total: ${total}</h2>
+    {user ? <Link to="/checkout" className="btn btn-primary btn-block">checkout</Link>
+      :
+      <Link to="/login" className="btn btn-primary btn-block">login</Link>
+    }
+  </section>
 }
