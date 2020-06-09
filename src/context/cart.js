3c361dd99d0921df1698d56ebe0be43c9e014307
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-09 16:13:09 +0700

    12. Calculate Cart Total and Cart Items

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,5 +1,5 @@
 // cart context
-import React from 'react';
+import React, { useEffect } from 'react';
 import localCart from '../utils/localCart';
 
 const CartContext = React.createContext();
@@ -8,7 +8,35 @@ function CartProvider({ children }) {
   const [cart, setCart] = React.useState(localCart);
   const [total, setTotal] = React.useState(0);
   const [cartItems, setCartItems] = React.useState(0);
-  return <CartContext.Provider value={{ cart, total, cartItems }}>
+
+  useEffect(() => {
+    // local storage
+    // cart items
+    let newCartItems = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount)
+    }, 0)
+    setCartItems(newCartItems);
+    // cart total
+    let newTotal = cart.reduce((total, cartItem) => {
+      return total += cartItem.amount * cartItem.price;
+    }, 0)
+    newTotal = parseFloat(newTotal.toFixed(2));
+    console.log(newTotal);
+    setTotal(newTotal);
+  }, [cart]);
+
+  //remove item 
+  const removeItem = id => { };
+  //increase amount 
+  const increaseAmount = id => { };
+  //decrease amount 
+  const decreaseAmount = id => { };
+  //add to cart
+  const addToCart = product => { };
+  //clear item 
+  const clearCart = () => { };
+
+  return <CartContext.Provider value={{ cart, total, cartItems, removeItem, increaseAmount, decreaseAmount, addToCart, clearCart }}>
     {children}
   </CartContext.Provider>
 }
