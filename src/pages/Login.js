23d7f6c2a2dba7e400deecf7222b717e9d57d6b4
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-18 16:37:20 +0700

    20. Login  Page JSX

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,5 +22,39 @@ export default function Login() {
 
   const handleSubmit = async e => { };
 
-  return <h1>hello from login page</h1>;
+  return (
+    <section className="form section">
+      <h2 className="section-title">{isMember ? "sign in" : "register"}</h2>
+      <form className="login-form">
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="email">email</label>
+          <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+        </div>
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </div>
+        {/* single input */}
+        {!isMember && <div className="form-control">
+          <label htmlFor="username">username</label>
+          <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} />
+        </div>}
+        {/* empty form text */}
+        {
+          isEmpty && <p className="form-empty">please fill out all form fields</p>
+        }
+        {/* submit btn */}
+        {!isEmpty && <button type="submit" className="btn btn-block btn-primary" onClick={handleSubmit}>submit</button>}
+        {/* register link */}
+        <p className="register-link">
+          {isMember ? "need to register" : "already a membter"}
+          <button type="button" onClick={toggleMember}>
+            click here
+          </button>
+        </p>
+      </form>
+    </section>
+  )
 }
