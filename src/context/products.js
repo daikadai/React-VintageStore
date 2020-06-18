bdec18c858453df498b44c46fedfa546df6b51e4
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-18 14:39:33 +0700

    17. Local Image Fix

--- src/context/products.js
+++ src/context/products.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 import url from '../utils/URL';
-import { featuredProducts } from '../utils/helpers';
+import { featuredProducts, flattenProducts } from '../utils/helpers';
 export const ProductContext = React.createContext();
 
 // Provider, Consumer
@@ -16,11 +16,12 @@ export default function ProductProvider({ children }) {
     setLoading(true);
     axios.get(`${url}/products`)
       .then(response => {
-        const featured = featuredProducts(response.data);
-        setProducts(response.data);
+        const featured = featuredProducts(flattenProducts(response.data));
+        const products = flattenProducts(response.data);
+        setProducts(products);
         setFeatured(featured);
         setLoading(false);
-        console.log(response.data);
+        console.log(products);
 
       });
     return () => {
--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,8 +1,4 @@
 // url
-export default "https://johnsmilgatutorials.com/projects/react-tech-store-v2"
+export default "http://localhost:1337"
 
-// install axios
-
-// get url
-
-// response 
+//local fix
