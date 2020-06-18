414029a02d01c2a47bede5ab87c2d93b63f279f1
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-18 16:21:15 +0700

    19. Login Page Setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,26 @@
 import React from "react";
 
+// strapi function
+
+// handle user
+import { useHistory } from "react-router-dom";
+
+
 export default function Login() {
+  const history = useHistory();
+  // setup user context
+
+  //state values
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState('default');
+  const [isMember, setIsMember] = React.useState(true);
+
+  let isEmpty = false;
+
+  const toggleMember = () => { };
+
+  const handleSubmit = async e => { };
+
   return <h1>hello from login page</h1>;
 }
