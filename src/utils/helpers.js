2fa99c45e57031a3fec4f10e9e9d0be783ae10fb
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-06-23 10:45:12 +0700

    46. Paginate Function - Slice Method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25,8 +25,13 @@ export function paginate(products) {
   const itemsPerPage = 4;
   const numberOfPages = Math.ceil(products.length / itemsPerPage);
 
-  const newProducts = Array.from({ length: numberOfPages }, () => {
-    return products.splice(0, itemsPerPage);
+  // const newProducts = Array.from({ length: numberOfPages }, () => {
+  //   return products.splice(0, itemsPerPage);
+  // })
+
+  const newProducts = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return products.slice(start, start + itemsPerPage);
   })
 
   console.log(newProducts);
